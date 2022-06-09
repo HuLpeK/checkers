7402e803b331f7b1828a5cfa8cdfee609c14192b
HuLpeK <hubipl42@gmail.com>
2022-06-09 17:30:02 +0200

    Add - toUpperCase test

--- tst/Participant_test.cc
+++ tst/Participant_test.cc
@@ -18,3 +18,28 @@ protected:
     void TearDown() override {};
 };
 
+
+TEST_F(ParticipantTest, Test_Player_ToUpperCaseTest_Should_Return_Char_In_UpperIndex)
+{
+    std::vector<char> expected;
+    expected.reserve(('Z'-'A')+('z'-'a'));
+    for(char i = 'A'; i <= 'Z'; i++)
+        expected.push_back(i);
+    for(char i = 'A'; i <= 'Z'; i++)
+        expected.push_back(i);
+    expected.shrink_to_fit();
+
+    std::vector<char> ans;
+    ans.reserve(('Z'-'A')+('z'-'a'));
+    for(char i = 'A'; i <= 'Z'; i++)
+        ans.push_back(Player::toUpperCase(i));
+    for(char i = 'a'; i <= 'z'; i++)
+        ans.push_back(Player::toUpperCase(i));
+    ans.shrink_to_fit();
+
+    EXPECT_EQ(ans.size(),expected.size());
+    for(int i = 0; i < ans.size(); i++)
+        EXPECT_EQ(ans[i],expected[i]);
+
+
+}
