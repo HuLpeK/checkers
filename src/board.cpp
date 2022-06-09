9692a7af23597c97b8192a8c7dc18e5aade334ae
HuLpeK <hubipl42@gmail.com>
2022-06-09 16:30:54 +0200

    Rewrite operator<< for board.cpp

--- src/board.cpp
+++ src/board.cpp
@@ -20,50 +20,44 @@ Board::Board() {
 
     int index = 0;
     for(int i = 0; i < SIZE; i += 2)
-        at(i,index) = Man(WHITE), at(i,index+2) = Man(WHITE);
+        at(i,index) = Man(BLACK), at(i,index+2) = Man(BLACK);
     index++;
     for(int i = 1; i < SIZE; i += 2)
-        at(i,index) = Man(WHITE);
+        at(i,index) = Man(BLACK);
 
     index = 7;
 
     for(int i = 1; i < SIZE; i += 2)
-        at(i, index) = Man(BLACK), at(i,index-2) = Man(BLACK);
+        at(i, index) = Man(WHITE), at(i,index-2) = Man(WHITE);
     index--;
     for(int i = 0; i < SIZE; i += 2)
-        at(i,index) = Man(BLACK);
+        at(i,index) = Man(WHITE);
 
 }
 
 std::ostream &operator<<(std::ostream &out,const Board &Bo) {
-    system("clear"); //todo change to smth more inteligent
+    system("clear"); //todo change to smth more elegant
 
     std::string tab[SIZE+3][SIZE+3];
     for(int i = 0; i <= SIZE; i++)
     {
-        tab[0][i+1] = 'A' + i;
-        tab[i][0] = '0' + i;
+        tab[SIZE][i+1] = 'A' + i;
+        tab[i-1][0] = '9' - i;
     }
-    tab[0][0] = " ";
-    for(int i = SIZE-1; i >= 0; i--)
+    tab[SIZE][0] = " ";
+    for(int i = 0; i < SIZE ; i++)
     {
         for(int j = 0; j < SIZE; j++)
-            tab[i+1][j+1] = Bo.at(j,i).getIcon();
+            tab[i][j+1] = Bo.at(j,i).getIcon();
     }
 
-    for(int i = SIZE; i >= 0; i--)
+    for(int i = 0; i <= SIZE ; i++)
     {
         for(int j = 0; j <= SIZE; j++)
             out << tab[i][j];
         out << "\n";
     }
 
-//    for(int i = SIZE-1; i >= 0; i--)
-//    {
-//        for(int j = 0; j < SIZE; j++)
-//            out << Bo.at(j,i).getIcon() << "";
-//        out << "\n";
-//    }
     return out;
 }
 
