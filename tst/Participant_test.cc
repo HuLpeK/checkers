0dfc6e1f3ce609aa84311ca14dbceeb8d75fa813
HuLpeK <hubipl42@gmail.com>
2022-06-09 17:16:14 +0200

    Create Participant_test.cc

---
+++ tst/Participant_test.cc
@@ -0,0 +1,19 @@
+
+/**
+ * \file Participant_test
+ */
+#include <gtest/gtest.h>
+#include "gmock/gmock.h"
+#include "../src/Participant.hpp"
+
+class ParticipantTest : public ::testing::Test {
+protected:
+    ParticipantTest() = default;;
+
+    ~ParticipantTest() override = default;;
+
+    void SetUp() override {};
+
+    void TearDown() override {};
+};
+
