361a8831d45c51fc961bcd1cb1c8f834b4ff215b
HuLpeK <hubipl42@gmail.com>
2022-06-14 11:30:25 +0200

    Add - put game_test.cc in logic_test.cc

--- tst/logic_test.cc
+++ tst/logic_test.cc
@@ -171,3 +171,50 @@ TEST(LogicTest, Check_If_Base_Board_Is_Bicie_Possible_Should_Return_Not)
     std::pair<int,int> expected {-1,-1};
     EXPECT_TRUE(expected == Logic::possibleBicie(bo,WHITE) and expected == Logic::possibleBicie(bo,BLACK));
 }
+
+TEST(LogicTest,Check_Test_From_RealLife)
+{
+    Board bo;
+    clear(bo);
+    std::vector<std::pair<std::string,int>> whitePos = {
+            {"A", 1},
+            {"C", 1},
+            {"E", 1},
+            {"G", 1},
+            {"B", 2},
+            {"D", 2},
+            {"F", 2},
+            {"H", 2},
+            {"E", 3},
+            {"G", 3},
+            {"B", 4},
+            {"D", 4}};
+    std::vector<std::pair<std::string,int>> blackPos = {
+            {"C",5},
+            {"D",6},
+            {"F",6},
+            {"H",6},
+            {"A",7},
+            {"C",7},
+            {"E",7},
+            {"G",7},
+            {"B",8},
+            {"D",8},
+            {"F",8},
+            {"H",8}
+    };
+    for(const auto& [x,y] : whitePos)
+    bo.at((x[0]-'A'),y-1) = Man(WHITE);
+
+    for(const auto& [x,y] : blackPos)
+    bo.at(x[0]-'A',y-1) = Man(BLACK);
+
+    std::pair<char,int> fr {'D',4};
+    std::pair<char,int> to {'A',6};
+    std::pair<std::pair<int,int>,std::pair<int,int>> mv {
+            {fr.first-'A',fr.second-1},
+            {to.first-'A',to.second-1}
+    };
+
+    EXPECT_TRUE(Logic::legalMove(bo,mv,WHITE));
+}
